24190582f5a3ae4cfd2f167d8e19cd7f53e19805
Code Sandbox <hello@codesandbox.io>
2022-07-21 17:22:28 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.64553c2583de555c84403ec853ce7072.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9db03f45097881fa02e96f0414642118",
+    "url": "https://clinicm.github.io/csb-730t0z/index.html"
+  },
+  {
+    "revision": "e94dd233ae135862ea3a",
+    "url": "https://clinicm.github.io/csb-730t0z/static/css/2.5a2813f3.chunk.css"
+  },
+  {
+    "revision": "a4d7328d0c085e726ecf",
+    "url": "https://clinicm.github.io/csb-730t0z/static/css/main.909fc2b9.chunk.css"
+  },
+  {
+    "revision": "e94dd233ae135862ea3a",
+    "url": "https://clinicm.github.io/csb-730t0z/static/js/2.6424fd49.chunk.js"
+  },
+  {
+    "revision": "8d3d787d0184d5c408ff40ba5ece50a3",
+    "url": "https://clinicm.github.io/csb-730t0z/static/js/2.6424fd49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4d7328d0c085e726ecf",
+    "url": "https://clinicm.github.io/csb-730t0z/static/js/main.219f6f1b.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "https://clinicm.github.io/csb-730t0z/static/js/main.219f6f1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aefd8d01efd49a5cd45b",
+    "url": "https://clinicm.github.io/csb-730t0z/static/js/runtime-main.34fb2b81.js"
+  },
+  {
+    "revision": "fd4db0ff3f66f964fdb24f60ddab5a4a",
+    "url": "https://clinicm.github.io/csb-730t0z/static/media/banner_1.fd4db0ff.jpg"
+  },
+  {
+    "revision": "f77e8fe9582f13fbd17999b876c1fe60",
+    "url": "https://clinicm.github.io/csb-730t0z/static/media/banner_2.f77e8fe9.jpg"
+  },
+  {
+    "revision": "67bb10f2c266820f7f68e150896f87bc",
+    "url": "https://clinicm.github.io/csb-730t0z/static/media/banner_3.67bb10f2.jpg"
+  },
+  {
+    "revision": "18a6bbdff932ced377d8031490bd3555",
+    "url": "https://clinicm.github.io/csb-730t0z/static/media/deal_ofthe_week.18a6bbdf.png"
+  },
+  {
+    "revision": "0280082048c8b0a72b8a7bfd582d114b",
+    "url": "https://clinicm.github.io/csb-730t0z/static/media/emptyCart.02800820.png"
+  },
+  {
+    "revision": "abebef23e38854b0a7ad161380471493",
+    "url": "https://clinicm.github.io/csb-730t0z/static/media/empty_cart.abebef23.png"
+  },
+  {
+    "revision": "dc7970a3ddb8a96a0f4f74bd17d05c24",
+    "url": "https://clinicm.github.io/csb-730t0z/static/media/slider_1.dc7970a3.jpg"
+  },
+  {
+    "revision": "a5bc9e893257dc0c821b846e32c7170d",
+    "url": "https://clinicm.github.io/csb-730t0z/static/media/slider_2.a5bc9e89.jpg"
+  },
+  {
+    "revision": "035f02ed5a4bc5be22aff5b9e6a1c3d1",
+    "url": "https://clinicm.github.io/csb-730t0z/static/media/slider_3.035f02ed.jpg"
+  }
+]);
